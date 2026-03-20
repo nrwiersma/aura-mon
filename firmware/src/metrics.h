fd4a80153576216ea74c2ab39b6f822ae0302e29
Nicholas Wiersma <nick@wiersma.co.za>
2026-03-20 12:43:31 +0200

    feat: better metrics

--- firmware/src/metrics.h
+++ firmware/src/metrics.h
@@ -11,7 +11,8 @@ struct PromMetrics {
     std::atomic<uint32_t> modbus_errors_total{0};
     std::atomic<uint64_t> modbus_collect_time_ms_total{0};
     std::atomic<uint32_t> modbus_last_run_avg_ms{0};
-    std::atomic<uint32_t> datalog_io{0};
+    std::atomic<uint32_t> datalog_read_io{0};
+    std::atomic<uint32_t> datalog_write_io{0};
     std::atomic<uint32_t> datalog_write_time_ms_total{0};
     std::atomic<uint32_t> datalog_cache_hit{0};
 };
